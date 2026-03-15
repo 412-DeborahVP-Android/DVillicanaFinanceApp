b4281a0b30401d79d47e95278f2577184a871b3a
deborahvp12 <deborahvillicana@outlook.com>
2026-03-14 18:09:40 -0600

    Encabezado implementado

---
+++ app/src/main/java/com/example/dvillicaafinanceapp/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.dvillicaafinanceapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LeadingIconTab
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dvillicaafinanceapp.ui.theme.DVillicañaFinanceAppTheme
+import com.example.dvillicaafinanceapp.ui.theme.naranjaPastel
+import com.example.dvillicaafinanceapp.ui.theme.verdePastel
+import com.example.dvillicaafinanceapp.ui.theme.moradoPastel
+import com.example.dvillicaafinanceapp.ui.theme.fondo
+
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DVillicañaFinanceAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun HomeScreen(innerPadding: PaddingValues){
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(fondo)
+            .padding(paddingValues = innerPadding)
+    ) {
+        //Encabezado
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp)
+                .padding(start = 20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(naranjaPastel)
+                    .height(70.dp)
+                    .width(70.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    imageVector =Icons.Default.AccountCircle,
+                    contentDescription = "Usuario",
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+            }
+            Column() {
+                Text(text = "Hola, Deborah",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold)
+                Text(text = "Bienvenido",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 10.dp))
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(
+                onClick = {},
+                modifier = Modifier.padding(end = 20.dp)
+                ){
+                Icon(
+                    imageVector =Icons.Default.Menu,
+                    contentDescription = "Menú",
+                    modifier = Modifier.size(35.dp)
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    DVillicañaFinanceAppTheme {
+        HomeScreen(innerPadding = PaddingValues())
+    }
+}
---
+++ app/src/main/java/com/example/dvillicaafinanceapp/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.dvillicaafinanceapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val naranjaPastel = Color(0xFFF1E4D9)
+
+val verdePastel = Color(0xFFE6F2ED)
+
+val moradoPastel = Color(0xFFE5E0F6)
+
+val fondo = Color(0xFFFAFAFF)
+
